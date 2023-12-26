64a84b98add2990e630bae1eb12331c37c490e75
Nicholas Gower <nicholasgower01@gmail.com>
2023-12-25 18:00:21 -0800

    Created simple api from tutorial

--- seattlecontratunes/user_api/urls.py
+++ seattlecontratunes/user_api/urls.py
@@ -1,19 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from django.urls import path, include
+from .views import UserViewSet, GroupViewSet
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model=User
-        fields= ["url","username","email","is_staff"]
-    
-class UserViewSet(viewsets.ModelViewSet):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
 
 router = routers.DefaultRouter()
 
 router.register(r'users',UserViewSet)
+router.register(r'groups',GroupViewSet)
 
 urlpatterns=[
     path('',include(router.urls)),
