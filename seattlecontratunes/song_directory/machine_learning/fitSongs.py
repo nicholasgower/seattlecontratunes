fe5cbf95443ab8a7edd385933a1bdf647189be2a
Nicholas Gower <nicholasgower01@gmail.com>
2023-12-30 14:40:35 -0800

    Initial experiments with scikit-learn

---
+++ seattlecontratunes/song_directory/machine_learning/fitSongs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 29 14:57:17 2023
+
+@author: Nicholas
+"""
+from .models import Song
+import scikit-learn
+from sklearn.ensemble import RandomForestClassifier
+
+
+def fitSongs():
+    songs=Song.abc.objects.all()
+    clf=RandomForestClassifier(random_state=0)
+    
+    clf.fit(songs.values_list("name",flat=True),songs.values_list("abc",flat=True))
+    
+    
+    
