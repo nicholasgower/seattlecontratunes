25ee1953e2d962e0bbea457167ac8ddc2e65bb98
Nicholas Gower <nicholasgower01@gmail.com>
2023-12-30 14:40:21 -0800

    Added test case to confirm that relationship between Medleys and Tunes is functional

--- seattlecontratunes/song_directory/tests.py
+++ seattlecontratunes/song_directory/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import Song, Medley
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class MedleyTuneTestCase(TestCase):
+    
+    def setUp(self):
+        User.objects.create(id=1)
+        Song.objects.create(name="One",abc="test",uploader_id=1)
+        Medley.objects.create(Tune1="One",uploader_id=1)
+        pass
+    def test_medley_tune_to_tune(self):
+        ''' This test seeks to test if when a tune is referenced in a medley,
+        it will find its matching tune in the Song database.
+        
+        
+        
+        '''
+        for MedleyInstance in Medley.objects.all():
+            for Tune in Medley.getTuneNames(id=MedleyInstance.pk):
+                #print(len(Song.objects.filter(name__icontains=Tune)))
+                self.assertEqual(len(Song.objects.filter(name__icontains=Tune))>0,True)
+    
