dc6716cb777d30f420e2693b008b393292cf580b
Nicholas Gower <nicholasgower01@gmail.com>
2024-01-27 12:25:43 -0800

    Misc insignificant changes

--- seattlecontratunes/assets/index.js
+++
@@ -1,7 +0,0 @@
-function component(){
-    const element = document.createElement("div");
-    ElementInternals.innerHTML= "Hello webpack";
-    return element;
-
-}
-document.body.appendChild(component());
